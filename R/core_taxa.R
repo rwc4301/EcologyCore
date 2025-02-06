5f6652e50bfa6f41bc37ab2475cd23528aeea2e2
Reuben Duncan <reuben.duncan25@outlook.com>
2025-02-06 20:21:44 +0000

    Update

--- R/core_taxa.R
+++ R/core_taxa.R
@@ -13,22 +13,226 @@
 
 # Adapted from https://github.com/ShadeLab/PAPER_Shade_CurrOpinMicro
 # input dataset needs to be rarefied and the rarefaction depth included
-core_occupancy_abundance_plot <- function(physeq, nReads=1000) {
-  otu <- as.data.frame(phyloseq::otu_table(physeq))
-  map <- as.data.frame(phyloseq::sample_data(physeq))
+# nReads=1000
 
-  otu_PA <- 1*((otu>0)==1)                                               # presence-absence data
-  otu_occ <- rowSums(otu_PA)/ncol(otu_PA)                                # occupancy calculation
-  otu_rel <- apply(decostand(otu, method="total", MARGIN=2),1, mean)     # mean relative abundance
-  occ_abun <- data.frame(otu_occ=otu_occ, otu_rel=otu_rel) %>%           # combining occupancy and abundance data frame
+# Class Descriptions for EcologyCore Core (ECC):
+## ECCOccupancyAbundance
+## ECCRankedSimilarity
+core_occupancy_abundance_plot <- function(physeq) {
+  abund_table <- as.data.frame(phyloseq::otu_table(physeq))
+  meta_table <- as.data.frame(phyloseq::sample_data(physeq))
+
+  # TODO: clean up hypothesis testing
+  label="Abstraction_Method"
+  meta_table$Groups <- as.factor(as.character(paste("Run ", meta_table$run, sep = "")))
+
+  meta_table$Type <- NULL
+  meta_table$Type2 <- NULL
+  meta_table$Connections <- NULL
+
+  meta_table <- meta_table[meta_table$Groups == "Run 1",]
+  ###
+
+  #Adjust abund_table to contain only those rows that got selected in the Hypothesis space
+  abund_table<-abund_table[rownames(meta_table),]
+  #After adjustment, get rid of OTUs that are all empty
+  abund_table<-abund_table[,colSums(abund_table)>0]
+
+  abund_table <- t(abund_table)
+
+  otu_PA <- 1*((abund_table>0)==1)                                              # presence-absence data
+  otu_occ <- rowSums(otu_PA)/ncol(otu_PA)                                       # occupancy calculation
+  otu_rel <- apply(decostand(abund_table, method="total", MARGIN=2),1, mean)    # mean relative abundance
+
+  occ_abund_table <- data.frame(otu_occ=otu_occ, otu_rel=otu_rel) %>%           # combining occupancy and abundance data frame
     rownames_to_column('otu')
 
-  # Occupancy abundance plot:
-  p <- ggplot(data=occ_abun, aes(x=log10(otu_rel), y=otu_occ)) +
-    geom_point(pch=21, fill='white') +
-    labs(x="log10(mean relative abundance)", y="Occupancy")
+  # Collate response and set class name
+  res <- list(abund_table = abund_table, occ_abund_table = occ_abund_table)
+  class(res) <- "ECCOccupancyAbundance"
 
-  return (p)
+  return (res)
+}
+
+plot.ECCOccupancyAbundance <- function(value) {
+  # Occupancy abundance plot:
+  p <- ggplot(data = value$occ_abund_table, aes(x = log10(otu_rel), y = otu_occ)) +
+    geom_point(pch = 21, fill = 'white') +
+    labs(x = "log10(mean relative abundance)", y = "Occupancy")
+
+  print(p)
+}
+
+similarity_ranked <- function(abund_table, occ_abund_table) {
+  otu <- abund_table
+
+  PresenceSum <- data.frame(otu = as.factor(row.names(otu)), otu) %>%
+    gather(sequence_name, abun, -otu) %>%
+    left_join(map, by = 'sequence_name') %>%
+    group_by(otu, sampling_date) %>%
+    summarise(time_freq=sum(abun>0)/length(abun),            # frequency of detection between time points
+              coreTime=ifelse(time_freq == 1, 1, 0)) %>%     # 1 only if occupancy 1 with specific time, 0 if not
+    group_by(otu) %>%
+    summarise(sumF=sum(time_freq),
+              sumG=sum(coreTime),
+              nS=length(sampling_date),
+              Index=(sumF+sumG)/nS)                 # calculating weighting Index based on number of time points detected and
+
+  otu_ranked <- occ_abund_table %>%
+    left_join(PresenceSum, by='otu') %>%
+    transmute(otu=otu,                           # check what transmute function does
+              rank=Index) %>%
+    arrange(desc(rank))
+
+  # Calculating the contribution of ranked OTUs to the BC similarity
+  BCaddition <- NULL
+
+  # calculating BC dissimilarity based on the 1st ranked OTU
+  otu_start=otu_ranked$otu[1]
+  start_matrix <- as.matrix(otu[otu_start,])
+  start_matrix <- t(start_matrix)
+  x <- apply(combn(ncol(start_matrix), 2), 2, function(x) sum(abs(start_matrix[,x[1]]- start_matrix[,x[2]]))/(2*nReads))
+  x_names <- apply(combn(ncol(start_matrix), 2), 2, function(x) paste(colnames(start_matrix)[x], collapse=' - '))
+  df_s <- data.frame(x_names,x)
+  names(df_s)[2] <- 1
+  BCaddition <- rbind(BCaddition,df_s)
+  # calculating BC dissimilarity based on addition of ranked OTUs from 2nd to 500th.
+  # Can be set to the entire length of OTUs in the dataset, however it might take
+  # some time if more than 5000 OTUs are included.
+  for(i in 2:500){
+    otu_add=otu_ranked$otu[i]
+    add_matrix <- as.matrix(otu[otu_add,])
+    add_matrix <- t(add_matrix)
+    start_matrix <- rbind(start_matrix, add_matrix)
+    x <- apply(combn(ncol(start_matrix), 2), 2, function(x) sum(abs(start_matrix[,x[1]]-start_matrix[,x[2]]))/(2*nReads))
+    x_names <- apply(combn(ncol(start_matrix), 2), 2, function(x) paste(colnames(start_matrix)[x], collapse=' - '))
+    df_a <- data.frame(x_names,x)
+    names(df_a)[2] <- i
+    BCaddition <- left_join(BCaddition, df_a, by=c('x_names'))
+  }
+  # calculating the BC dissimilarity of the whole dataset (not needed if the second loop is already including all OTUs)
+  x <-  apply(combn(ncol(otu), 2), 2, function(x) sum(abs(otu[,x[1]]-otu[,x[2]]))/(2*nReads))
+  x_names <- apply(combn(ncol(otu), 2), 2, function(x) paste(colnames(otu)[x], collapse=' - '))
+  df_full <- data.frame(x_names,x)
+  names(df_full)[2] <- length(rownames(otu))
+  BCfull <- left_join(BCaddition,df_full, by='x_names')
+
+  rownames(BCfull) <- BCfull$x_names
+  temp_BC <- BCfull
+  temp_BC$x_names <- NULL
+  temp_BC_matrix <- as.matrix(temp_BC)
+
+  BC_ranked <- data.frame(rank = as.factor(row.names(t(temp_BC_matrix))),t(temp_BC_matrix)) %>%
+    gather(comparison, BC, -rank) %>%
+    group_by(rank) %>%
+    summarise(MeanBC=mean(BC)) %>%            # mean Bray-Curtis dissimilarity
+    arrange(desc(-MeanBC)) %>%
+    mutate(proportionBC=MeanBC/max(MeanBC))   # proportion of the dissimilarity explained by the n number of ranked OTUs
+  Increase=BC_ranked$MeanBC[-1]/BC_ranked$MeanBC[-length(BC_ranked$MeanBC)]
+  increaseDF <- data.frame(IncreaseBC=c(0,(Increase)), rank=factor(c(1:(length(Increase)+1))))
+  BC_ranked <- left_join(BC_ranked, increaseDF)
+  BC_ranked <- BC_ranked[-nrow(BC_ranked),]
+
+  #Creating thresholds for core inclusion
+
+  #Method:
+  #A) Elbow method (first order difference) (script modified from https://pommevilla.github.io/random/elbows.html)
+  fo_difference <- function(pos){
+    left <- (BC_ranked[pos, 2] - BC_ranked[1, 2]) / pos
+    right <- (BC_ranked[nrow(BC_ranked), 2] - BC_ranked[pos, 2]) / (nrow(BC_ranked) - pos)
+    return(left - right)
+  }
+  BC_ranked$fo_diffs <- sapply(1:nrow(BC_ranked), fo_difference)
+
+  elbow <- which.max(BC_ranked$fo_diffs)
+
+  #B) Final increase in BC similarity of equal or greater then 2%
+  lastCall <- last(as.numeric(as.character(BC_ranked$rank[(BC_ranked$IncreaseBC>=1.02)])))
+
+  # Collate response and set class name
+  res <- list(BC_ranked = BC_ranked, elbow = elbow, lastCall = lastCall, )
+  class(res) <- "ECCRankedSimilarity"
+
+  return (res)
+}
+
+plot.ECCRankedSimilarity <- function(value) {
+  BC_ranked <- value$BC_ranked
+  elbow <- value$elbow
+
+  #Creating plot of Bray-Curtis similarity
+  p <- ggplot(BC_ranked[1:100,], aes(x=factor(BC_ranked$rank[1:100], levels=BC_ranked$rank[1:100]))) +
+    geom_point(aes(y=proportionBC)) +
+    theme_classic() + theme(strip.background = element_blank(),axis.text.x = element_text(size=7, angle=45)) +
+    geom_vline(xintercept=elbow, lty=3, col='red', cex=.5) +
+    geom_vline(xintercept=lastCall, lty=3, col='blue', cex=.5) +
+    labs(x='Ranked Taxa',y='Contribution to Bray-Curtis Dissimilarity') +
+    annotate(geom="text", x=elbow+14, y=.1, label=paste("Elbow method"," (",elbow,")", sep=''), color="red")+
+    annotate(geom="text", x=lastCall+3, y=.5, label=paste("Last 2% increase (",lastCall,")",sep=''), color="blue")
+
+  print(p)
+}
+
+exercise_1 <- function() {
+  #' Exercise 1:
+  #' Create a vector with all "core" OTUs.
+
+  # Use Sloan neutral model to prioritize OTUs
+  # Fitting neutral model (Burns et al., 2016 (ISME J) - functions are in the sncm.fit.R)
+  spp=t(otu)
+  taxon=as.vector(rownames(otu))
+
+  #Models for the whole community
+  obs.np=sncm.fit(spp, taxon, stats=FALSE, pool=NULL)
+  sta.np=sncm.fit(spp, taxon, stats=TRUE, pool=NULL)
+
+  above.pred=sum(obs.np$freq > (obs.np$pred.upr), na.rm=TRUE)/sta.np$Richness  # fraction of OTUs above prediction
+  below.pred=sum(obs.np$freq < (obs.np$pred.lwr), na.rm=TRUE)/sta.np$Richness  # fraction of OTUs below prediction
+
+  #Create a column defining "core" OTUs
+  occ_abun$fill <- 'no'
+  occ_abun$fill[occ_abun$otu %in% otu_ranked$otu[1:lastCall]] <- 'core'
+
+  p3 <- ggplot() +
+    geom_point(data=occ_abun[occ_abun$fill=='no',], aes(x=log10(otu_rel), y=otu_occ), pch=21, fill='white', alpha=.2)+
+    geom_point(data=occ_abun[occ_abun$fill!='no',], aes(x=log10(otu_rel), y=otu_occ), pch=21, fill='blue', size=1.8) +
+    geom_line(color='black', data=obs.np, size=1, aes(y=obs.np$freq.pred, x=log10(obs.np$p)), alpha=.25) +
+    geom_line(color='black', lty='twodash', size=1, data=obs.np, aes(y=obs.np$pred.upr, x=log10(obs.np$p)), alpha=.25)+
+    geom_line(color='black', lty='twodash', size=1, data=obs.np, aes(y=obs.np$pred.lwr, x=log10(obs.np$p)), alpha=.25)+
+    labs(x="log10(mean relative abundance)", y="Occupancy")
+}
+
+exercise_two <- function() {
+  #' Exercise 2:
+  #' Highlight on the same occupancy-abundance plot OTUs that are above and below
+  #' the natural model prediction.
+  #' Extra: add to the plot the above.pred and below.pred values
+
+
+  #Creating a plot of core taxa occupancy by time point
+  core <- occ_abun$otu[occ_abun$fill == 'core']
+
+  otu_relabun <- decostand(otu, method="total", MARGIN=2)
+
+  plotDF <- data.frame(otu = as.factor(row.names(otu_relabun)), otu_relabun) %>%
+    gather(sequence_name, relabun, -otu) %>%
+    left_join(map, by = 'sequence_name') %>%
+    left_join(otu_ranked, by='otu') %>%
+    filter(otu %in% core) %>%
+    group_by(otu, sampling_date) %>%
+    summarise(time_freq=sum(relabun>0)/length(relabun),
+              coreTime=ifelse(time_freq == 1, 1, 0),
+              detect=ifelse(time_freq > 0, 1, 0))
+
+  plotDF$otu <- factor(plotDF$otu, levels=otu_ranked$otu[1:34])
+
+  p4 <- ggplot(plotDF,aes(x=otu, time_freq,fill=factor(sampling_date))) +
+    geom_bar(stat = 'identity', position = 'dodge') +
+    coord_flip() +
+    scale_x_discrete(limits = rev(levels(plotDF$otu))) +
+    theme(axis.text = element_text(size=6)) +
+    labs(x='Ranked OTUs', y='Occupancy by site', fill="Sampling date") +
+    scale_fill_npg()
 }
 
 core_taxa <- function(abund_table, meta_table, OTU_taxonomy, OTU_tree, N = 25) {
