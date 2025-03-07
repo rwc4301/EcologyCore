87e447cba2d16de5e9d3a57d300ea49b9c5da820
Reuben Duncan <reuben.duncan25@outlook.com>
2025-03-07 13:09:48 +0000

    Added parallelisation to NRI & NTI

--- R/environmental_filtering.R
+++ R/environmental_filtering.R
@@ -3,28 +3,20 @@
 #to give an account of stochastic versus deterministic nature of microbial communities
 #v1.2 (All metrics are now being saved)
 
-# library(phyloseq)
-# library(ape)
-# library(picante)
-# library(data.table)
-# library(ggplot2)
-# library(grid) #We need grid to draw the arrows
-
-environmental_filtering <- function(abund_table, meta_table, OTU_taxonomy, OTU_tree) {
-  # TODO: remove
-  grouping_column <- "Groups"
+environmental_filtering <- function(physeq, runs = 999, iterations = 1000, top_n = 2000, abundance.weighted = FALSE, null.model = c("taxa.labels", "richness", "frequency", "sample.pool", "phylogeny.pool", "independentswap", "trialswap")) {
+  null.model <- match.arg(null.model)
 
   # Process input data
-  # abund_table <- phyloseq::otu_table(physeq)
-  # meta_table <- phyloseq::sample_data(physeq)
-  # OTU_taxonomy <- phyloseq::tax_table(physeq)
-  # OTU_tree <- phyloseq::phy_tree(physeq)
+  abund_table <- phyloseq::otu_table(physeq)
+  meta_table <- phyloseq::sample_data(physeq)
+  OTU_taxonomy <- phyloseq::tax_table(physeq)
+  OTU_tree <- phyloseq::phy_tree(physeq)
 
   #We extract N most abundant OTUs
-  abund_table<-abund_table[,order(colSums(abund_table),decreasing=TRUE)][,1:min(Top_N_abundant_OTUs,dim(abund_table)[2])]
+  abund_table<-abund_table[,order(colSums(abund_table),decreasing=TRUE)][,1:min(top_n,dim(abund_table)[2])]
 
   #Adjust OTU_tree
-  OTU_tree<-drop.tip(OTU_tree,OTU_tree$tip.label[!OTU_tree$tip.label %in% colnames(abund_table)])
+  OTU_tree<-ape::drop.tip(OTU_tree,OTU_tree$tip.label[!OTU_tree$tip.label %in% colnames(abund_table)])
 
 
   #Sometimes the filtering will remove the OTUs and these won't exist in the OTU_tree
@@ -32,20 +24,19 @@ environmental_filtering <- function(abund_table, meta_table, OTU_taxonomy, OTU_t
 
   #There is a bug in adespatial as it doesn't like phyloseq's class and won't calculate
   #the values, so I am forcing it to become matrix
-  abund_table<-as(abund_table,"matrix")
+  comm <- as(abund_table,"matrix")
+  dist <- cophenetic(OTU_tree)
 
   #Reference http://kembellab.ca/r-workshop/biodivR/SK_Biodiversity_R.html
-  abund_table.sesmpd <- ses.mpd(abund_table, cophenetic(OTU_tree),  null.model = null.model, abundance.weighted = abundance.weighted,
-                                runs = runs, iterations=iterations)
-  abund_table.sesmntd <- ses.mntd(abund_table, cophenetic(OTU_tree),  null.model = null.model, abundance.weighted = abundance.weighted,
-                                  runs = runs, iterations=iterations)
+  abund_table.sesmpd <- ses.mpd(comm, dist, null.model = null.model, abundance.weighted = abundance.weighted, runs = runs, iterations = iterations)
+  abund_table.sesmntd <- ses.mntd(comm, dist, null.model = null.model, abundance.weighted = abundance.weighted, runs = runs, iterations = iterations)
 
   #Write all the metrics in a file for further analyses elsewhere
   data_to_write<-data.frame(abund_table.sesmpd[,"mpd.obs.z",drop=F],abund_table.sesmntd[,"mntd.obs.z",drop=F])
   #Convert SES scores to NRI/NTI
   data_to_write<-data_to_write*-1
   colnames(data_to_write)<-c("NRI","NTI")
-  write.csv(data_to_write,file=paste("Environmental_Filtering","_",second_label,"_",label,"_",null.model,".csv",sep=""))
+  #write.csv(data_to_write,file=paste("Environmental_Filtering","_",second_label,"_",label,"_",null.model,".csv",sep=""))
   #/Write all the metrics in a file for further analyses elsewhere
 
   #Positive SES values (abund_table.sesmpd$mpd.obs.z > 0) and high quantiles (abund_table.sesmpd$mpd.obs.p > 0.95)
@@ -134,5 +125,107 @@ environmental_filtering <- function(abund_table, meta_table, OTU_taxonomy, OTU_t
     names(df_pw)<-c("measure","from","to","y","p")
   }
 
-  return(structure(list(df = df, df_pw = df_pw, pval = pval, meta_table = meta_table), className = "ECEnvironmentalFiltering"))
+  return(structure(list(df = df, df_pw = df_pw, pval = pval, meta_table = meta_table, data_to_write = data_to_write), className = "ECEnvironmentalFiltering"))
+}
+
+#' @import picante
+ses.mpd <- function(
+    samp,
+    dis,
+    null.model = c("taxa.labels", "richness", "frequency", "sample.pool", "phylogeny.pool", "independentswap", "trialswap"),
+    abundance.weighted = FALSE,
+    runs = 999,
+    iterations = 1000,
+    ncores = parallel::detectCores() - 1
+) {
+  dis <- as.matrix(dis)
+  mpd.obs <- mpd(samp, dis, abundance.weighted = abundance.weighted)
+  null.model <- match.arg(null.model)
+
+  # Setup parallel backend
+  cl <- parallel::makeCluster(ncores)
+  parallel::clusterExport(cl, c("mpd", "taxaShuffle", "randomizeMatrix", "iterations"))
+
+  # Parallel computation of null model
+  mpd.rand <- switch(null.model,
+    taxa.labels = t(parallel::parSapply(cl, 1:runs, function(x) mpd(samp, taxaShuffle(dis), abundance.weighted = abundance.weighted))),
+    richness = t(parallel::parSapply(cl, 1:runs, function(x) mpd(randomizeMatrix(samp, null.model = "richness"), dis, abundance.weighted))),
+    frequency = t(parallel::parSapply(cl, 1:runs, function(x) mpd(randomizeMatrix(samp, null.model = "frequency"), dis, abundance.weighted))),
+    sample.pool = t(parallel::parSapply(cl, 1:runs, function(x) mpd(randomizeMatrix(samp, null.model = "richness"), dis, abundance.weighted))),
+    phylogeny.pool = t(parallel::parSapply(cl, 1:runs, function(x) mpd(randomizeMatrix(samp, null.model = "richness"), taxaShuffle(dis), abundance.weighted))),
+    independentswap = t(parallel::parSapply(cl, 1:runs, function(x) mpd(randomizeMatrix(samp, null.model = "independentswap", iterations), dis, abundance.weighted))),
+    trialswap = t(parallel::parSapply(cl, 1:runs, function(x) mpd(randomizeMatrix(samp, null.model = "trialswap", iterations), dis, abundance.weighted)))
+  )
+
+  parallel::stopCluster(cl)
+
+  # Calculate statistics
+  mpd.rand.mean <- apply(X = mpd.rand, MARGIN = 2, FUN = mean, na.rm = TRUE)
+  mpd.rand.sd <- apply(X = mpd.rand, MARGIN = 2, FUN = sd, na.rm = TRUE)
+  mpd.obs.z <- (mpd.obs - mpd.rand.mean)/mpd.rand.sd
+  mpd.obs.rank <- apply(X = rbind(mpd.obs, mpd.rand), MARGIN = 2, FUN = rank)[1, ]
+  mpd.obs.rank <- ifelse(is.na(mpd.rand.mean), NA, mpd.obs.rank)
+
+  return (data.frame(
+    ntaxa = specnumber(samp),
+    mpd.obs,
+    mpd.rand.mean,
+    mpd.rand.sd,
+    mpd.obs.rank,
+    mpd.obs.z,
+    mpd.obs.p = mpd.obs.rank/(runs + 1),
+    runs = runs,
+    row.names = row.names(samp)
+  ))
+}
+
+#' @import picante
+ses.mntd <- function(
+  samp,
+  dis,
+  null.model = c("taxa.labels", "richness", "frequency", "sample.pool", "phylogeny.pool", "independentswap", "trialswap"),
+  abundance.weighted = FALSE,
+  runs = 999,
+  iterations = 1000,
+  ncores = parallel::detectCores() - 1
+) {
+  dis <- as.matrix(dis)
+  mntd.obs <- mntd(samp, dis, abundance.weighted)
+  null.model <- match.arg(null.model)
+
+  # Setup parallel backend
+  cl <- parallel::makeCluster(ncores)
+  parallel::clusterExport(cl, c("mntd", "taxaShuffle", "randomizeMatrix", "iterations"))
+
+  # Parallel computation of null model
+  mntd.rand <- switch(null.model,
+    taxa.labels = t(parallel::parSapply(cl, 1:runs, function(x) mntd(samp, taxaShuffle(dis), abundance.weighted))),
+    richness = t(parallel::parSapply(cl, 1:runs, function(x) mntd(randomizeMatrix(samp, null.model = "richness"), dis, abundance.weighted))),
+    frequency = t(parallel::parSapply(cl, 1:runs, function(x) mntd(randomizeMatrix(samp, null.model = "frequency"), dis, abundance.weighted))),
+    sample.pool = t(parallel::parSapply(cl, 1:runs, function(x) mntd(randomizeMatrix(samp, null.model = "richness"), dis, abundance.weighted))),
+    phylogeny.pool = t(parallel::parSapply(cl, 1:runs, function(x) mntd(randomizeMatrix(samp, null.model = "richness"), taxaShuffle(dis), abundance.weighted))),
+    independentswap = t(parallel::parSapply(cl, 1:runs, function(x) mntd(randomizeMatrix(samp, null.model = "independentswap", iterations), dis, abundance.weighted))),
+    trialswap = t(parallel::parSapply(cl, 1:runs, function(x) mntd(randomizeMatrix(samp, null.model = "trialswap", iterations), dis, abundance.weighted)))
+  )
+
+  parallel::stopCluster(cl)
+
+  # Calculate statistics
+  mntd.rand.mean <- apply(X = mntd.rand, MARGIN = 2, FUN = mean, na.rm = TRUE)
+  mntd.rand.sd <- apply(X = mntd.rand, MARGIN = 2, FUN = sd, na.rm = TRUE)
+  mntd.obs.z <- (mntd.obs - mntd.rand.mean)/mntd.rand.sd
+  mntd.obs.rank <- apply(X = rbind(mntd.obs, mntd.rand), MARGIN = 2, FUN = rank)[1, ]
+  mntd.obs.rank <- ifelse(is.na(mntd.rand.mean), NA, mntd.obs.rank)
+
+  return (data.frame(
+    ntaxa = specnumber(samp),
+    mntd.obs,
+    mntd.rand.mean,
+    mntd.rand.sd,
+    mntd.obs.rank,
+    mntd.obs.z,
+    mntd.obs.p = mntd.obs.rank/(runs + 1),
+    runs = runs,
+    row.names = row.names(samp)
+  ))
 }
